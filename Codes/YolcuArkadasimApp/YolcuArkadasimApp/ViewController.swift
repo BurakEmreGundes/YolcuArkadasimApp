fb3474c5288d78ea02df3f198218419179d4e2e4
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-03-04 17:42:24 +0300

    Kullanıcı Giriş Ve Kayıt
    Kullanıcı giriş ve kayıt işlemleri yapıldı. Rota yönlendirmesi yapıldı

--- Codes/YolcuArkadasimApp/YolcuArkadasimApp/ViewController.swift
+++ Codes/YolcuArkadasimApp/YolcuArkadasimApp/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("sea")
+       
     }
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != ""{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authdata, error) in
+                if error != nil{
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Hatalı İşlem")
+                }else{
+                    self.performSegue(withIdentifier: "toTabBarVC", sender: nil)
+                }
+            }
+        }else{
+            self.makeAlert(title: "Hata", message: "Lütfen boş olan bırakmayınız")
+        }
+    }
+    
+    func makeAlert(title:String,message:String){
+        let alert=UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton=UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert,animated:true,completion: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != ""{
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authdata, error) in
+                if error != nil{
+                    self.makeAlert(title: "OK", message: error?.localizedDescription ?? "Hatalı İşlem")
+                }else{
+                    self.performSegue(withIdentifier: "toTabBarVC", sender: nil)
+                }
+            }
+            
+        }else{
+            self.makeAlert(title: "Error", message: "Lütfen alanları boş bırakmayınız")
+        }
+    }
+    
 
 
 }
