10baf8126dda4d6307868455093253d31ad4835d
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-03-08 16:08:33 +0300

    LastTravelVC güncellendi
    Sayfa yapısı oluşturuldu fake veri ile beslendi

--- Codes/YolcuArkadasimApp/YolcuArkadasimApp/LastTravelViewController.swift
+++ Codes/YolcuArkadasimApp/YolcuArkadasimApp/LastTravelViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class LastTravelViewController: UIViewController {
+class LastTravelViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
 
+    @IBOutlet weak var tableView: UITableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate=self
+        tableView.dataSource=self
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TravelTableViewCell
+        return cell
+    }
+    
+    
     
 
     /*
---
+++ Codes/YolcuArkadasimApp/YolcuArkadasimApp/TravelTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TravelTableViewCell.swift
+//  YolcuArkadasimApp
+//
+//  Created by Burak Emre gündeş on 8.03.2022.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
